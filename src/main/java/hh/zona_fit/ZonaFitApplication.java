4913d90482508efe54582135026ec89c8302896e
Harold Herrera <hrldrmnd@gmail.com>
2025-02-18 13:01:56 -0600

    agrege scaner

--- src/main/java/hh/zona_fit/ZonaFitApplication.java
+++ src/main/java/hh/zona_fit/ZonaFitApplication.java
@@ -3,11 +3,14 @@ package hh.zona_fit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ZonaFitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZonaFitApplication.class, args);
+		var scan=new Scanner(System.in);
 	}
-	//Subir a gitHub
+
 }
